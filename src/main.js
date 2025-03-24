6ebf8d9d5b7d630f332b16e8f681171864a87b65
Zaved <zavedahmadca707@gmail.com>
2025-03-25 03:34:44 +0530

    certain changes to ui added message bubbles

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
 import './style.css'
-function sendMessage(){
+console.log('script loaded')
+function sendMessage(content){
+    let text=document.getElementById('textbox').value
+    let container=document.getElementById('messageContainer')
+    const scroller=document.getElementById('scroller');
+    container.innerHTML += `
+    <div class="chat chat-end">
+        <div class="rounded-4xl chat-bubble chat-bubble-secondary">${text}</div>
+        </div>
+`;
+
     
+    console.log('button pressed',text)
+    scroller.scrollTop = scroller.scrollHeight
 }
+
+function handleDOMContentLoaded() {
+    var sendButton = document.getElementById("sendButton");
+    if (sendButton) {
+      sendButton.addEventListener("click", sendMessage);
+    } else {
+      console.error("Send button not found!");
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", handleDOMContentLoaded)
